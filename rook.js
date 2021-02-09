3dbef897f1d019d2ec942b233e952876404c2d74
LalexCunningham <lalexcunningham@gmail.com>
2021-02-09 20:21:21 +0000

    Simplified moves and redentant nested loops

--- rook.js
+++ rook.js
@@ -10,94 +10,66 @@ export default class Rook extends Piece {
 		let attackingMoves = []
 
 		// Move in positive Y
-		let axisBlocked = false;
 		for (let i = this.currentCoordinates[1] + 1; i < 9; i++) {
-			for (let piece of this.player.activePieces) {
-				if (this.currentCoordinates[0] === piece.currentCoordinates[0] && i === piece.currentCoordinates[1]) {
-					axisBlocked = true;
-					break;
-				}
-			}
-			if (!axisBlocked) {
-				for (let piece of this.enemyPlayer.activePieces) {
-					if (this.currentCoordinates[0] === piece.currentCoordinates[0] && i === piece.currentCoordinates[1]) {
-						attackingMoves.push([[this.currentCoordinates[0], i], piece])
-						axisBlocked = true;
-						break;
-					}
-				}
-			}
-			if (!axisBlocked) {
-				possibleMoves.push([this.currentCoordinates[0], i]);
+
+			let coordinate = [this.currentCoordinates[0], i];
+			let square = this.board.pieceLocations[coordinate[0] - 1][coordinate[1] - 1];
+
+			if (square === null) {
+				possibleMoves.push(coordinate);
+			} else if (square.color === this.color) {
+				break;
+			} else {
+				attackingMoves.push([coordinate, square]);
+				break;
 			}
 		}
 
 		// Move in positive X
-		axisBlocked = false;
 		for (let i = this.currentCoordinates[0] + 1; i < 9; i++) {
-			for (let piece of this.player.activePieces) {
-				if (this.currentCoordinates[1] === piece.currentCoordinates[1] && i === piece.currentCoordinates[0]) {
-					axisBlocked = true;
-					break;
-				}
-			}
-			if (!axisBlocked) {
-				for (let piece of this.enemyPlayer.activePieces) {
-					if (this.currentCoordinates[1] === piece.currentCoordinates[1] && i === piece.currentCoordinates[0]) {
-						attackingMoves.push([[i, this.currentCoordinates[1]], piece])
-						axisBlocked = true;
-						break;
-					}
-				}
-			}
-			if (!axisBlocked) {
-				possibleMoves.push([i, this.currentCoordinates[1]]);
+
+			let coordinate = [i, this.currentCoordinates[1]];
+			let square = this.board.pieceLocations[coordinate[0] - 1][coordinate[1] - 1];
+
+			if (square === null) {
+				possibleMoves.push(coordinate);
+			} else if (square.color === this.color) {
+				break;
+			} else {
+				attackingMoves.push([coordinate, square]);
+				break;
 			}
 		}
 
 		// Move in negative Y
-		axisBlocked = false;
 		for (let i = this.currentCoordinates[1] - 1; i > 0; i--) {
-			for (let piece of this.player.activePieces) {
-				if (this.currentCoordinates[0] === piece.currentCoordinates[0] && i === piece.currentCoordinates[1]) {
-					axisBlocked = true;
-					break;
-				}
-			}
-			if (!axisBlocked) {
-				for (let piece of this.enemyPlayer.activePieces) {
-					if (this.currentCoordinates[0] === piece.currentCoordinates[0] && i === piece.currentCoordinates[1]) {
-						attackingMoves.push([[this.currentCoordinates[0], i], piece])
-						axisBlocked = true;
-						break;
-					}
-				}
-			}
-			if (!axisBlocked) {
-				possibleMoves.push([this.currentCoordinates[0], i]);
+
+			let coordinate = [this.currentCoordinates[0], i];
+			let square = this.board.pieceLocations[coordinate[0] - 1][coordinate[1] - 1];
+
+			if (square === null) {
+				possibleMoves.push(coordinate);
+			} else if (square.color === this.color) {
+				break;
+			} else {
+				attackingMoves.push([coordinate, square]);
+				break;
 			}
 		}
 
 		// Move in negative X
-		axisBlocked = false;
 		for (let i = this.currentCoordinates[0] - 1; i > 0; i--) {
-			for (let piece of this.player.activePieces) {
-				if (this.currentCoordinates[1] === piece.currentCoordinates[1] && i === piece.currentCoordinates[0]) {
-					axisBlocked = true;
-					break;
-				}
-			}
-			if (!axisBlocked) {
-				for (let piece of this.enemyPlayer.activePieces) {
-					if (this.currentCoordinates[1] === piece.currentCoordinates[1] && i === piece.currentCoordinates[0]) {
-						attackingMoves.push([[i, this.currentCoordinates[1]], piece]);
-						axisBlocked = true;
-						break;
-					}
-				}
-			}
-			if (!axisBlocked) {
-				possibleMoves.push([i, this.currentCoordinates[1]]);
+
+			let coordinate = [i, this.currentCoordinates[1]];
+			let square = this.board.pieceLocations[coordinate[0] - 1][coordinate[1] - 1];
+
+			if (square === null) {
+				possibleMoves.push(coordinate);
+			} else if (square.color === this.color) {
+				break;
+			} else {
+				attackingMoves.push([coordinate, square]);
+				break;
 			}
 		}
 		return [possibleMoves, attackingMoves];
@@ -106,85 +78,69 @@ export default class Rook extends Piece {
 	getCheckingMoves () {
 		let moves = [];
 
-		// Move in positive Y
-		let axisBlocked = false;
 		for (let i = this.currentCoordinates[1] + 1; i < 9; i++) {
-			for (let piece of this.player.activePieces) {
-				if (this.currentCoordinates[0] === piece.currentCoordinates[0] && i === piece.currentCoordinates[1]) {
-					moves.push([[this.currentCoordinates[0], i], piece])
-					axisBlocked = true;
-					break;
-				}
-			}
-			if (!axisBlocked) {
-				for (let piece of this.enemyPlayer.activePieces) {
-					if (this.currentCoordinates[0] === piece.currentCoordinates[0] && i === piece.currentCoordinates[1]) {
-						axisBlocked = true;
-						break;
-					}
-				}
+
+			let coordinate = [this.currentCoordinates[0], i];
+			let square = this.board.pieceLocations[coordinate[0] - 1][coordinate[1] - 1];
+
+			if (square === null) {
+				moves.push(coordinate);
+			} else if (square.color === this.color) {
+				moves.push(coordinate);
+				break;
+			} else {
+				break;
 			}
 		}
 
 		// Move in positive X
-		axisBlocked = false;
 		for (let i = this.currentCoordinates[0] + 1; i < 9; i++) {
-			for (let piece of this.player.activePieces) {
-				if (this.currentCoordinates[1] === piece.currentCoordinates[1] && i === piece.currentCoordinates[0]) {
-					moves.push([[i, this.currentCoordinates[1]], piece])
-					axisBlocked = true;
-					break;
-				}
-			}
-			if (!axisBlocked) {
-				for (let piece of this.enemyPlayer.activePieces) {
-					if (this.currentCoordinates[1] === piece.currentCoordinates[1] && i === piece.currentCoordinates[0]) {
-						axisBlocked = true;
-						break;
-					}
-				}
+
+			let coordinate = [i, this.currentCoordinates[1]];
+			let square = this.board.pieceLocations[coordinate[0] - 1][coordinate[1] - 1];
+
+			if (square === null) {
+				moves.push(coordinate);
+			} else if (square.color === this.color) {
+				moves.push(coordinate);
+				break;
+			} else {
+				break;
 			}
 		}
 
 		// Move in negative Y
-		axisBlocked = false;
 		for (let i = this.currentCoordinates[1] - 1; i > 0; i--) {
-			for (let piece of this.player.activePieces) {
-				if (this.currentCoordinates[0] === piece.currentCoordinates[0] && i === piece.currentCoordinates[1]) {
-					moves.push([[this.currentCoordinates[0], i], piece])
-					axisBlocked = true;
-					break;
-				}
-			}
-			if (!axisBlocked) {
-				for (let piece of this.enemyPlayer.activePieces) {
-					if (this.currentCoordinates[0] === piece.currentCoordinates[0] && i === piece.currentCoordinates[1]) {
-						axisBlocked = true;
-						break;
-					}
-				}
+
+			let coordinate = [this.currentCoordinates[0], i];
+			let square = this.board.pieceLocations[coordinate[0] - 1][coordinate[1] - 1];
+
+			if (square === null) {
+				moves.push(coordinate);
+			} else if (square.color === this.color) {
+				moves.push(coordinate);
+				break;
+			} else {
+				break;
 			}
 		}
 
 		// Move in negative X
-		axisBlocked = false;
 		for (let i = this.currentCoordinates[0] - 1; i > 0; i--) {
-			for (let piece of this.player.activePieces) {
-				if (this.currentCoordinates[1] === piece.currentCoordinates[1] && i === piece.currentCoordinates[0]) {
-					moves.push([[i, this.currentCoordinates[1]], piece]);
-					axisBlocked = true;
-					break;
-				}
-			}
-			if (!axisBlocked) {
-				for (let piece of this.enemyPlayer.activePieces) {
-					if (this.currentCoordinates[1] === piece.currentCoordinates[1] && i === piece.currentCoordinates[0]) {
-						axisBlocked = true;
-						break;
-					}
-				}
+
+			let coordinate = [i, this.currentCoordinates[1]];
+			let square = this.board.pieceLocations[coordinate[0] - 1][coordinate[1] - 1];
+
+			if (square === null) {
+				moves.push(coordinate);
+			} else if (square.color === this.color) {
+				moves.push(coordinate);
+				break;
+			} else {
+				break;
 			}
 		}
+
 		return moves;
 	} 
 }
