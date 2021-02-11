b6c5185eca39090917ce670bc01f4bc61c2fd7af
LalexCunningham <lalexcunningham@gmail.com>
2021-02-11 10:43:03 +0000

    Removed old code

--- knight.js
+++ knight.js
@@ -41,24 +41,6 @@ export default class Knight extends Piece {
 				possibleMoves.splice(i, 1);
 				continue;
 			}
-			// if ((possibleMoves[i][0] > 8) || (possibleMoves[i][0] < 1) || (possibleMoves[i][1] > 8) || (possibleMoves[i][1] < 1)) {
-			// 	possibleMoves.splice(i, 1);
-			// 	continue;
-			// }
-			// for (let piece of this.player.activePieces) {
-			// 	if (piece.currentCoordinates[0] === possibleMoves[i][0] && piece.currentCoordinates[1] === possibleMoves[i][1]) {
-			// 		possibleMoves.splice(i, 1);
-			// 		continue loopKnightsMoves;
-			// 	}
-			// }
-			// for (let piece of this.enemyPlayer.activePieces) {
-				
-			// 	if (piece.currentCoordinates[0] === possibleMoves[i][0] && piece.currentCoordinates[1] === possibleMoves[i][1]) {
-			// 		attackingMoves.push([possibleMoves[i], piece]);
-			// 		possibleMoves.splice(i, 1);
-			// 		continue loopKnightsMoves;
-			// 	}
-			// }
 		}
 		return [possibleMoves, attackingMoves]
 	}	
